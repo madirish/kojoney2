b3eac5719278898081a045b7f78c04c1045e2190
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2012-08-15 10:44:24 -0400

    Updated honeypot and added conditional root prompt.

--- kojoney.py
+++ kojoney.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 """
+    Modified by Justin C. Klein Keane <jukeane@sas.upenn.edu>
+    Last updated: April 26, 2012
+
     Kojoney - A honeypot that emules a secure shell (SSH) server.
     Copyright (C) 2005 Jose Antonio Coret
 
@@ -42,7 +45,11 @@ from coret_log import *
 # First of all. Start logging now()!
 #
 start_logging()
+# This line magically logs auth attempts to the database via mod in userauth.py in modified Twisted
 userauth.conch_mysql_connect(DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_NAME)
+
+# Global holder
+FAKE_USERNAME = ""
 
 """
 Running our fake shell over an SSH channel.
@@ -51,8 +58,10 @@ Log in with username "user" and password "password".
 class CoretAvatar(avatar.ConchUser):
 
     def __init__(self, username):
+        global FAKE_USERNAME
         avatar.ConchUser.__init__(self)
         self.username = username
+        FAKE_USERNAME = username
         self.channelLookup.update({'session':session.SSHSession})
 
 class CoretRealm:
@@ -80,7 +89,9 @@ class CoretProtocol(protocol.Protocol):
 
 
     def connectionMade(self):
-        print self.transport
+        global FAKE_USERNAME
+        self.fake_username = FAKE_USERNAME
+        #self.transport.write('howdy ' + FAKE_USERNAME + '!\r\n\r\n')
         self.transport.write('Welcome to ' + str(FAKE_OS) + '!\r\n\r\n' +str(FAKE_PROMPT))
 
     #Modified by Martin Barbella for database support,
@@ -88,7 +99,8 @@ class CoretProtocol(protocol.Protocol):
     #removal of line breaks from commands (to prevent logs from being broken).
     def dataReceived(self, data):
         global FAKE_PROMPT
-
+        if self.fak_username == 'root':
+            FAKE_PROMPT = string.replace(FAKE_PROMPT, '$', '#')
         if data == '\r':
             self.lastCmd = string.replace(self.lastCmd, '\r', '')
             self.lastCmd = string.replace(self.lastCmd, '\n', '')
@@ -97,7 +109,7 @@ class CoretProtocol(protocol.Protocol):
             escaped_command = connection.escape_string(self.lastCmd)
             escaped_ip = connection.escape_string(self.transport.session.conn.transport.transport.getPeer()[1])
             cursor.execute("INSERT INTO executed_commands SET command='%s', ip='%s'" % (escaped_command, escaped_ip))
-            retvalue = processCmd(self.lastCmd, self.transport)
+            retvalue = processCmd(self.lastCmd, self.transport, self.fake_username, escaped_ip)
             self.lastCmd = ""
             #data = '\r\n' + str(FAKE_PROMPT) 
             
@@ -134,9 +146,20 @@ class CoretProtocol(protocol.Protocol):
 
         self.transport.write(data)
 
-publicKey = FAKE_SSH_KEY
+publicKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAGEArzJx8OYOnJmzf4tfBEvLi8DVPrJ3/c9k2I/Az64fxjHf9imyRJbixtQhlH9lfNjUIx+4LmrJH5QNRsFporcHDKOTwTTYLh5KmRpslkYHRivcJSkbh/C+BR3utDS555mV'
 
-privateKey = FAKE_SSH_PRIVKEY
+privateKey = """-----BEGIN RSA PRIVATE KEY-----
+MIIByAIBAAJhAK8ycfDmDpyZs3+LXwRLy4vA1T6yd/3PZNiPwM+uH8Yx3/YpskSW
+4sbUIZR/ZXzY1CMfuC5qyR+UDUbBaaK3Bwyjk8E02C4eSpkabJZGB0Yr3CUpG4fw
+vgUd7rQ0ueeZlQIBIwJgbh+1VZfr7WftK5lu7MHtqE1S1vPWZQYE3+VUn8yJADyb
+Z4fsZaCrzW9lkIqXkE3GIY+ojdhZhkO1gbG0118sIgphwSWKRxK0mvh6ERxKqIt1
+xJEJO74EykXZV4oNJ8sjAjEA3J9r2ZghVhGN6V8DnQrTk24Td0E8hU8AcP0FVP+8
+PQm/g/aXf2QQkQT+omdHVEJrAjEAy0pL0EBH6EVS98evDCBtQw22OZT52qXlAwZ2
+gyTriKFVoqjeEjt3SZKKqXHSApP/AjBLpF99zcJJZRq2abgYlf9lv1chkrWqDHUu
+DZttmYJeEfiFBBavVYIF1dOlZT0G8jMCMBc7sOSZodFnAiryP+Qg9otSBjJ3bQML
+pSTqy7c3a2AScC/YyOwkDaICHnnD3XyjMwIxALRzl0tQEKMXs6hH8ToUdlLROCrP
+EhQ0wahUTCk1gKA4uPD6TMTChavbh4K63OvbKg==
+-----END RSA PRIVATE KEY-----"""
 
 class InMemoryPublicKeyChecker(SSHPublicKeyDatabase):
 
