d6ddf4dc4cf07584535c0d9755b2bbaaac6166df
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2012-08-15 11:27:15 -0400

    Updated to sent proper e-mail using mail and logrotate format.

--- reports/mailalert.bash
+++ reports/mailalert.bash
@@ -1,14 +1,19 @@
 #!/bin/bash
+# Modified by Justin C. Klein Keane <jukeane@sas.upenn.edu>
+# Last updated 15 Aug, 2012
 # This script will send a daily honeypot report to the configured email address
-DATE=`date '+%Y-%m-%d' --date='1 day ago'`
+#DATE=`date '+%Y-%m-%d' --date='1 day ago'`
 # email subject
-SUBJECT="Kojoney Daily Report"
+#SUBJECT="Kojoney Daily Report"
 # email to
 EMAIL="root@localhost"
 # email message
-EMAILMESSAGE="/usr/share/kojoney/report.txt"
+#EMAILMESSAGE="/usr/share/kojoney/report.txt"
 # generate report
-/usr/share/kojoney/kojreport-filter /var/log/honeypot.log $DATE 0 0 1 > $EMAILMESSAGE
+#/usr/share/kojoney/kojreport-filter /var/log/honeypot.log $DATE 0 0 1 > $EMAILMESSAGE
 # send email using /bin/mail
-/usr/bin/mutt -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
+#/usr/bin/mutt -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
 
+KOJLOG=/var/log/honeypot.log-$(date +%Y%m%d)
+/usr/share/kojoney/kojreport $KOJLOG | mail -s "Kojoney Report" $EMAIL
+logger Kojoney report generated and sent.
