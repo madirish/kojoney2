edb96cb1d9eebe02e48ed7692e033f18f9bc7a2a
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2012-08-20 13:47:54 -0400

    Added Kippo style mods to user auth

--- kojoney.py
+++ kojoney.py
@@ -2,7 +2,7 @@
 
 """
     Modified by Justin C. Klein Keane <jukeane@sas.upenn.edu>
-    Last updated: April 26, 2012
+    Last updated: August 20, 2012
 
     Kojoney - A honeypot that emules a secure shell (SSH) server.
     Copyright (C) 2005 Jose Antonio Coret
@@ -147,20 +147,9 @@ class CoretProtocol(protocol.Protocol):
 
         self.transport.write(data)
 
-publicKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAGEArzJx8OYOnJmzf4tfBEvLi8DVPrJ3/c9k2I/Az64fxjHf9imyRJbixtQhlH9lfNjUIx+4LmrJH5QNRsFporcHDKOTwTTYLh5KmRpslkYHRivcJSkbh/C+BR3utDS555mV'
+publicKey = FAKE_SSH_KEY
 
-privateKey = """-----BEGIN RSA PRIVATE KEY-----
-MIIByAIBAAJhAK8ycfDmDpyZs3+LXwRLy4vA1T6yd/3PZNiPwM+uH8Yx3/YpskSW
-4sbUIZR/ZXzY1CMfuC5qyR+UDUbBaaK3Bwyjk8E02C4eSpkabJZGB0Yr3CUpG4fw
-vgUd7rQ0ueeZlQIBIwJgbh+1VZfr7WftK5lu7MHtqE1S1vPWZQYE3+VUn8yJADyb
-Z4fsZaCrzW9lkIqXkE3GIY+ojdhZhkO1gbG0118sIgphwSWKRxK0mvh6ERxKqIt1
-xJEJO74EykXZV4oNJ8sjAjEA3J9r2ZghVhGN6V8DnQrTk24Td0E8hU8AcP0FVP+8
-PQm/g/aXf2QQkQT+omdHVEJrAjEAy0pL0EBH6EVS98evDCBtQw22OZT52qXlAwZ2
-gyTriKFVoqjeEjt3SZKKqXHSApP/AjBLpF99zcJJZRq2abgYlf9lv1chkrWqDHUu
-DZttmYJeEfiFBBavVYIF1dOlZT0G8jMCMBc7sOSZodFnAiryP+Qg9otSBjJ3bQML
-pSTqy7c3a2AScC/YyOwkDaICHnnD3XyjMwIxALRzl0tQEKMXs6hH8ToUdlLROCrP
-EhQ0wahUTCk1gKA4uPD6TMTChavbh4K63OvbKg==
------END RSA PRIVATE KEY-----"""
+privateKey = FAKE_SSH_PRIVKEY
 
 class InMemoryPublicKeyChecker(SSHPublicKeyDatabase):
 
@@ -215,14 +204,67 @@ class CoretFactory(factory.SSHFactory):
         t.factory = self
         return t
 
+#From Kippo via https://code.google.com/p/kojoney-patch/
+#
+#Update: Nov 2011 - Mehdi Poustchi Amin
+#
+class CoretPasswordChecker:
+    implements(checkers.ICredentialsChecker)
+
+    credentialInterfaces = (credentials.IUsernamePassword,
+        credentials.IPluggableAuthenticationModules)
+
+    def requestAvatarId(self, credentials):
+        if hasattr(credentials, 'password'):
+            if self.checkUserPass(credentials.username, credentials.password):
+                return defer.succeed(credentials.username)
+            else:
+                return defer.fail(error.UnauthorizedLogin())
+        elif hasattr(credentials, 'pamConversion'):
+            return self.checkPamUser(credentials.username,
+                credentials.pamConversion)
+        return defer.fail(error.UnhandledCredentials())
+        
+    def checkPamUser(self, username, pamConversion):
+        r = pamConversion((('Password:', 1),))
+        return r.addCallback(self.cbCheckPamUser, username)
+
+    def cbCheckPamUser(self, responses, username):
+        for response, zero in responses:
+            if self.checkUserPass(username, response):
+                return defer.succeed(username)
+        return defer.fail(error.UnauthorizedLogin())
+
+    def checkUserPass(self, username, password):
+        file = open("/etc/kojoney/fake_users", "r")
+        i = 0
+        success = False
+        for line in file:
+            i += 1
+        data = line.split(' ')
+        try:
+            if username == data[0] and password == data[1].rstrip():
+                success = True
+                break
+        except:
+            log.msg("Error in fake users file at line " + str(i))
+    
+        file.close()
+        if success:
+            log.msg('login attempt %s:%s succeeded' % (username, password))
+        else:
+            log.msg('login attempt %s:%s failed' % (username, password))
+        return success
+
 portal = portal.Portal(CoretRealm())
 
 #
 # Register the fake username and password
 #
-passwdDB = checkers.InMemoryUsernamePasswordDatabaseDontUse()
-add_users(passwdDB)
-portal.registerChecker(passwdDB)
+#passwdDB = checkers.InMemoryUsernamePasswordDatabaseDontUse()
+#add_users(passwdDB)
+#portal.registerChecker(passwdDB)
+portal.registerChecker(CoretPasswordChecker())
 portal.registerChecker(InMemoryPublicKeyChecker())
 
 CoretFactory.portal = portal
