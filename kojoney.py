ad0311d24f43dd814d565b701ae394bb94235642
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-01-16 14:15:19 -0500

    Corrected login error so usernames can have multiple passwords, added dynamic directory creation, dynamic home directories, more dynamic directory changing, and root versus regular user prompts

--- kojoney.py
+++ kojoney.py
@@ -2,10 +2,9 @@
 
 """
     Modified by Justin C. Klein Keane <jukeane@sas.upenn.edu>
-    Last updated: January 15, 2013
+    Last updated: January 16, 2013
 
     Kojoney - A honeypot that emules a secure shell (SSH) server.
-    Copyright (C) 2005 Jose Antonio Coret
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -45,8 +44,6 @@ from coret_log import *
 # First of all. Start logging now()!
 #
 start_logging()
-# This line magically logs auth attempts to the database via mod in userauth.py in modified Twisted
-#userauth.conch_mysql_connect(DATABASE_USER, DATABASE_PASS, DATABASE_HOST, DATABASE_NAME)
 
 # Global holder
 FAKE_USERNAME = ""
@@ -63,6 +60,7 @@ class CoretAvatar(avatar.ConchUser):
         self.username = username
         FAKE_USERNAME = username
         self.channelLookup.update({'session':session.SSHSession})
+        #self.channelLookup.update({'session': CoretSession})
 
 class CoretRealm:
     try:
@@ -86,21 +84,25 @@ class CoretProtocol(protocol.Protocol):
     This is our Coret protocol that we will run over SSH
     """
     lastCmd = ""
+    fake_workingdir = "/"
 
 
     def connectionMade(self):
-        global FAKE_USERNAME, FAKE_PROMPT
+        global FAKE_USERNAME, FAKE_PROMPT, FAKE_CWD, FAKE_HOMEDIRS
         self.fake_username = FAKE_USERNAME
+        if FAKE_USERNAME in FAKE_HOMEDIRS:
+            self.fake_workingdir = FAKE_HOMEDIRS[FAKE_USERNAME]
+        else:
+            FAKE_CWD = "/"
         if self.fake_username == 'root':
             FAKE_PROMPT = string.replace(FAKE_PROMPT, '$', '#')
-        #self.transport.write('howdy ' + FAKE_USERNAME + '!\r\n\r\n')
         self.transport.write('Welcome to ' + str(FAKE_OS) + '!\r\n\r\n' +str(FAKE_PROMPT))
 
     #Modified by Martin Barbella for database support,
     #backspace bug fix, arrow key bug fix (by ignoring arrow input),
     #removal of line breaks from commands (to prevent logs from being broken).
     def dataReceived(self, data):
-        global FAKE_PROMPT, FAKE_USERNAME
+        global FAKE_PROMPT, FAKE_USERNAME, FAKE_CWD
         
         if data == '\r':
             self.lastCmd = string.replace(self.lastCmd, '\r', '')
@@ -110,15 +112,19 @@ class CoretProtocol(protocol.Protocol):
             escaped_command = connection.escape_string(self.lastCmd)
             escaped_ip = connection.escape_string(self.transport.session.conn.transport.transport.getPeer()[1])
             cursor.execute("INSERT INTO executed_commands SET command='%s', ip='%s'" % (escaped_command, escaped_ip)) 
-            #retvalue = processCmd(self.lastCmd, self.transport, self.fake_username, escaped_ip)
-            retvalue = processCmd(self.lastCmd, self.transport, FAKE_USERNAME, escaped_ip)
+            retvalue = processCmd(self.lastCmd, self.transport, FAKE_USERNAME, escaped_ip, self.fake_workingdir)
             self.lastCmd = ""
-            #data = '\r\n' + str(FAKE_PROMPT) 
             
-            if retvalue != 0:
-                data = '\r\n'
+            (printlinebreak, ret_workingdir, ret_username) = retvalue
+            print "Working dir returned as " + ret_workingdir
+            self.fake_workingdir = ret_workingdir
+            FAKE_USERNAME = ret_username
+            if FAKE_USERNAME == 'root':
+                FAKE_PROMPT = string.replace(FAKE_PROMPT, '$', '#')
             else:
-                data = ""
+                FAKE_PROMPT = string.replace(FAKE_PROMPT, '#', '$')
+            if printlinebreak == 1:
+                data = '\r\n'
             
             data += str(FAKE_PROMPT)
         elif data == '\x03': #^C
@@ -186,10 +192,88 @@ class CoretSession:
 from twisted.python import components
 components.registerAdapter(CoretSession, CoretAvatar, session.ISession)
 
+# blatantly stolen from Kippo (and modified)    
+from twisted.conch.ssh.common import NS, getNS
+class HoneyPotSSHUserAuthServer(userauth.SSHUserAuthServer):
+    def serviceStarted(self):
+        userauth.SSHUserAuthServer.serviceStarted(self)
+        self.bannerSent = False
+
+    def sendBanner(self):
+        if self.bannerSent:
+            return
+        self.transport.sendPacket(userauth.MSG_USERAUTH_BANNER, NS(FAKE_SSH_SERVER_VERSION+'\r\n') + NS('en'))
+        self.bannerSent = True
+
+    def ssh_USERAUTH_REQUEST(self, packet):
+        self.sendBanner()
+        return userauth.SSHUserAuthServer.ssh_USERAUTH_REQUEST(self, packet)
+    
+# blatantly stolen from Kippo (and modified)
+class HoneypotPasswordChecker:
+    implements(checkers.ICredentialsChecker)
+
+    credentialInterfaces = (credentials.IUsernamePassword,
+        credentials.IPluggableAuthenticationModules)
+    
+    authorizedCredentials = {}
+    
+    def __init__(self):
+        file = open(FAKE_USERS_FILE, "r")
+        i = 0
+        for line in file:
+            i += 1
+            data = line.split(' ')
+            if data[0] in self.authorizedCredentials:
+                self.authorizedCredentials[data[0]] = self.authorizedCredentials[data[0]] + data[1].rstrip() + ','
+            else:
+                self.authorizedCredentials[data[0]] = data[1].rstrip() + ','
+        print "Loaded " + str(i) + " accounts from " + FAKE_USERS_FILE
+
+    def requestAvatarId(self, credentials):
+        if hasattr(credentials, 'password'):
+            if self.checkUserPass(credentials.username, credentials.password):
+                return defer.succeed(credentials.username)
+            else:
+                return defer.fail(error.UnauthorizedLogin())
+        elif hasattr(credentials, 'pamConversion'):
+            return self.checkPamUser(credentials.username,
+                credentials.pamConversion)
+        return defer.fail(error.UnhandledCredentials())
+
+    def checkPamUser(self, username, pamConversion):
+        r = pamConversion((('Password:', 1),))
+        return r.addCallback(self.cbCheckPamUser, username)
+
+    def cbCheckPamUser(self, responses, username):
+        for response, zero in responses:
+            if self.checkUserPass(username, response):
+                return defer.succeed(username)
+        return defer.fail(error.UnauthorizedLogin())
+
+    def checkUserPass(self, username, password):
+        if username in self.authorizedCredentials:
+            print self.authorizedCredentials[username]
+            passwords = self.authorizedCredentials[username].split(',')
+            if passwords.count(password) > 0:
+                print '%s authenticated with password' % (username)
+                print 'login attempt [%s/%s] succeeded' % (username, password)
+                return True
+            else:
+                print 'login attempt [%s/%s] failed' % (username, password)
+                return False
+        else:
+            print 'login attempt [%s/%s] failed' % (username, password)
+            return False
+
+
 class CoretFactory(factory.SSHFactory):
     publicKeys = {'ssh-rsa': keys.getPublicKeyString(data=publicKey)}
     privateKeys = {'ssh-rsa': keys.getPrivateKeyObject(data=privateKey)}
-    services = {'ssh-userauth': userauth.SSHUserAuthServer, 'ssh-connection': connection.SSHConnection}
+    services = {
+                'ssh-userauth': HoneyPotSSHUserAuthServer,
+                'ssh-connection': connection.SSHConnection
+                }
     
     def buildProtocol(self, addr):
         t = transport.SSHServerTransport()
@@ -206,15 +290,7 @@ class CoretFactory(factory.SSHFactory):
         return t
 
 portal = portal.Portal(CoretRealm())
-
-#
-# Register the fake username and password
-#
-passwdDB = checkers.InMemoryUsernamePasswordDatabaseDontUse()
-add_users(passwdDB)
-portal.registerChecker(passwdDB)
-portal.registerChecker(InMemoryPublicKeyChecker())
-
+portal.registerChecker(HoneypotPasswordChecker())
 CoretFactory.portal = portal
 
 #
