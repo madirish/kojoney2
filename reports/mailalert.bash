ddd10d75ffcf97a0036d335c81497e86232f8b84
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-01-28 12:30:29 -0500

    Removing comment cruft

--- reports/mailalert.bash
+++ reports/mailalert.bash
@@ -1,19 +1,14 @@
 #!/bin/bash
-# Modified by Justin C. Klein Keane <jukeane@sas.upenn.edu>
-# Last updated 15 Aug, 2012
-# This script will send a daily honeypot report to the configured email address
-#DATE=`date '+%Y-%m-%d' --date='1 day ago'`
-# email subject
-#SUBJECT="Kojoney Daily Report"
-# email to
+# This file is part of the Kojoney2 honeypot
+#
+# Main Developer - Justin C. Klein Keane <jukeane@sas.upenn.edu>
+# Original Developer - Jose Antonio Coret <joxeankoret@yahoo.es>
+# Last updated 28 January 2013
+#
+# This script will send a honeypot report to the configured email address
+#
 EMAIL="root@localhost"
-# email message
-#EMAILMESSAGE="/usr/share/kojoney/report.txt"
-# generate report
-#/usr/share/kojoney/kojreport-filter /var/log/honeypot.log $DATE 0 0 1 > $EMAILMESSAGE
-# send email using /bin/mail
-#/usr/bin/mutt -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
 
 KOJLOG=/var/log/honeypot.log-$(date +%Y%m%d)
-/usr/share/kojoney/kojreport $KOJLOG | mail -s "Kojoney Report" $EMAIL
-logger Kojoney report generated and sent.
+/usr/share/kojoney/kojreport $KOJLOG | mail -s "Kojoney2 Report" $EMAIL
+logger Kojoney2 report generated and sent.
