347fd2509c252e483b5a0c5a8cea8e7d5da55366
Justin C. Klein Keane <jukeane@sas.upenn.edu>
2013-01-30 13:17:03 -0500

    Added downloads to the table definitions

--- create_tables.sql
+++ create_tables.sql
@@ -23,3 +23,14 @@ CREATE TABLE IF NOT EXISTS `executed_commands` (
   `command` VARCHAR(100),
   `ip_numeric` INT(10) UNSIGNED
 );
+
+CREATE TABLE IF NOT EXISTS `downloads` (
+	`id` INT(12) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `time` TIMESTAMP,
+  `ip` VARCHAR(15),
+  `ip_numeric` INT(10) UNSIGNED,
+  `url` VARCHAR(100),
+  `md5sum` VARCHAR(32),
+  `filetype` VARCHAR(255),
+  `clamsig` text
+);
